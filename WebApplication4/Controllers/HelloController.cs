49cc4c3343eefdbf4db524417253ca935446d24f
orun-sunny <sunny.mukut@gmail.com>
2022-05-18 16:45:44 +0600

    first commit

---
+++ WebApplication4/Controllers/HelloController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+
+namespace WebApplication4.Controllers
+{
+
+
+    public class HelloController : ApiController
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult Post(JObject payload)
+        {
+            return 
+        }
+        
+
+
+    }
+}
